d64a9cee2844abf5424562cbf6737c50d91b2b53
Ferrel John Fernando <ferrel.j@flash-coffee.com>
2022-05-27 18:31:58 +0700

    add prisma

---
+++ grpc-todo-service/src/services/prisma.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  // * Still looking out what the different with onModuleDestroy()
+  // async enableShutdownHooks(app: INestApplication) {
+  //   this.$on('beforeExit', async () => {
+  //     await app.close();
+  //   });
+  // }
+}
